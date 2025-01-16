fa6841e3478d999ee39d17702a3f1982cd46a0e6
peruccii <peruccii2917@hotmail.com>
2025-01-16 17:54:31 -0300

    refatorate

--- apps/server/src/trpc/trpc.router.ts
+++ apps/server/src/trpc/trpc.router.ts
@@ -1,16 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { TrpcService } from './trpc.service';
-import { z } from 'zod';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { getNameService } from '@server/application/usecases/getname.service';
+import { schema } from '@server/application/zod/schemas/get-name.schema.ts';
 
 @Injectable()
 export class TrpcRouter {
-  constructor(private readonly trpc: TrpcService) {}
+  constructor(
+    private readonly trpc: TrpcService,
+    private readonly nameService: getNameService,
+  ) {}
 
   appRouter = this.trpc.router({
-    getName: this.trpc.procedure
-      .input(z.object({ name: z.string() }))
-      .query(({ input }) => {
-        return `Hello ${input.name}`;
-      }),
+    getName: this.trpc.procedure.input(schema).query(({ input }) => {
+      const a = {
+        name: input.name,
+        age: input.age,
+      };
+      return this.nameService.getName(a);
+    }),
   });
+
+  async applyMiddleware(app: INestApplication) {
+    app.use(
+      `/internal/trpc`,
+      trpcExpress.createExpressMiddleware({ router: this.appRouter }),
+    );
+  }
 }
+
+export type AppRouter = TrpcRouter['appRouter'];
