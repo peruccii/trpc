0274f679af80d95eee11ab1c3d1eda4736ea8de6
peruccii <peruccii2917@hotmail.com>
2025-01-17 21:02:23 -0300

    zod age schema

--- apps/server/src/application/zod/schemas/get-name.schema.ts
+++ apps/server/src/application/zod/schemas/get-name.schema.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-  name: z.string(),
-  age: z.number(),
+  age: z
+    .number()
+    .min(0, 'Your age must be at least 3 years old')
+    .max(80, 'Your age must be less than 80 years old'),
 });
 
-export type GetNameAndAgeSchema = z.infer<typeof schema>;
+export type TypeAge = z.infer<typeof schema>;
