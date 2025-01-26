f3d94970c230c4ff3b7aea619d59f7bf0e2dc052
peruccii <peruccii2917@hotmail.com>
2025-01-26 17:50:42 -0300

    mementori

--- apps/web/app/clientside.tsx
+++ apps/web/app/clientside.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { trpc } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { CustomInput } from "./ components/custom-input";
 
 export default function Months() {
   const [currentWeek, setCurrentWeek] = useState(0);
@@ -31,16 +32,19 @@ export default function Months() {
 
   return (
     <div className="w-full max-w-7xl mx-auto">
-      <input
-        onChange={(e) => handleTakeAge(Number(e.target.value))}
-        placeholder="Coloque sua idade aqui"
-        maxLength={3}
-      />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {Array.from({ length: Math.ceil(weeks.length / 27) }, (_, rowIndex) => (
+      <div className="items-center flex ">
+        <CustomInput
+          placeholder="Coloque sua idade aqui"
+          className="flex items-center mx-auto w-96 mb-7"
+          maxLength={3}
+          onChange={(e) => handleTakeAge(Number(e.target.value))}
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 gap-4">
+        {Array.from({ length: Math.ceil(weeks.length / 18) }, (_, rowIndex) => (
           <div key={`row-${rowIndex}`} className="flex flex-wrap">
-            {Array.from({ length: 27 }, (_, colIndex) => {
-              const weekNumber = rowIndex * 27 + colIndex + 1;
+            {Array.from({ length: 18 }, (_, colIndex) => {
+              const weekNumber = rowIndex * 18 + colIndex + 1;
               if (weekNumber > weeks.length) return null;
 
               return (
@@ -48,7 +52,7 @@ export default function Months() {
                   <div
                     key={colIndex}
                     className={`w-5 h-5 m-[1px] rounded-sm flex items-center justify-center text-[8px]
-                      border-2 border-white ${weekNumber <= currentWeek ? "bg-white" : "bg-black"}
+                      border-[0.5px] border-white ${weekNumber <= currentWeek ? "bg-white" : "bg-black"}
                        hover:bg-primary-foreground hover:scale-150 transition-all duration-200 ease-in-out`}
                     title={`Week ${weekNumber}`}
                   ></div>
